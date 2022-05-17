ee32dfd93b4d8dedc0e2ddafdd46858ea4647439
ammaret909 <ammaret_aituphon@cmu.ac.th>
2022-05-17 19:27:55 +0700

    fibo

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-let n = 3;
-let count_f=1;
-for(let i = 1 ; i <= n ; i++){
-    count_f = count_f * i
-}
-console.log(count_f)
 
-n = 3;
-let count_w = 1;
-while(n>0){
-    count_w *= n;
-    n--;
+let input = 6;
+let f1 = 0;
+let f2 = 1;
+let count_for = 1;
+for(let i=2; i<input; i++){
+    f2 = f1;
+    f1 = count_for;
+    count_for = f2 + f1;
 }
-console.log(count_w)
+console.log(count_for);
 
-console.log(count_w === count_f);
+input = 6;
+f1 = 0;
+f2 = 1;
+let count_while = 1;
+let i = 2;
+while(i<input){
+    f2 = f1;
+    f1 = count_while;
+    count_while = f2 + f1;
+    i++;
+}
+console.log(count_while);
+console.log(count_for===count_while);
