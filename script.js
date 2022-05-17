58e42e7cc26cf4b7f3ca8d8d36b06f10c8da3a02
ammaret909 <ammaret_aituphon@cmu.ac.th>
2022-05-17 19:33:00 +0700

    prime

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
 
-let input = 6;
-let f1 = 0;
-let f2 = 1;
-let count_for = 1;
-for(let i=2; i<input; i++){
-    f2 = f1;
-    f1 = count_for;
-    count_for = f2 + f1;
+let input = 11;
+let prime = true;
+for(let i=2; i*i<=input; i++){
+    if(input % i  == 0){
+        prime = false;
+        break
+    }
 }
-console.log(count_for);
-
-input = 6;
-f1 = 0;
-f2 = 1;
-let count_while = 1;
-let i = 2;
-while(i<input){
-    f2 = f1;
-    f1 = count_while;
-    count_while = f2 + f1;
-    i++;
-}
-console.log(count_while);
-console.log(count_for===count_while);
+console.log(prime);
