ea334841511369b83ecf5fd42641e43d6bb0f24b
ammaret909 <ammaret_aituphon@cmu.ac.th>
2022-05-17 21:54:31 +0700

    homework-prime

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 
-let input = 11;
-let prime = true;
-for(let i=2; i*i<=input; i++){
-    if(input % i  == 0){
-        prime = false;
-        break
+let input = 50;
+let prime;
+for(let i=2; i<=input; i++){
+    prime = 1;
+    for(j=2;j*j<=i;j++){
+        if(i%j == 0){
+            prime = 0;
+            break;
+        }
+    }
+    if(prime != 0){
+        console.log(i+" ");
     }
 }
-console.log(prime);
