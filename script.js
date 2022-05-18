45da22c152bd99de30f2034dbeae9a0838d4bd2b
ammaret909 <ammaret_aituphon@cmu.ac.th>
2022-05-18 12:14:18 +0700

    cancel to ok

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-let score = 79.4;
-let grade;
-if(score > 80){
-    grade = 'A';
-}
-else if(score >= 75){
-    grade = 'B';
-}
-else if(score >= 70){
-    grade = 'C';
-}
-else if(score >= 65){
-    grade = 'D';
-}
-else if(score >= 60){
-    grade = 'f';
-}
-console.log(grade +' '+ score);
+const newText = 'not ok';
+document.getElementById('ok').innerHTML = newText;
+const nt = 'ok';
+document.getElementById('cancel').innerHTML = nt;
